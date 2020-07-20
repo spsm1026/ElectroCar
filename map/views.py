7db2c4f4698194e053559c43b485c430f33c1421
ssea9977 <ssea9977@gmail.com>
2020-07-20 16:32:13 +0900

    tired

--- map/views.py
+++ map/views.py
@@ -4,6 +4,7 @@ from django.urls import path
 from django.template import loader
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 
@@ -17,17 +18,12 @@ def main(request):
     chargespot_list = []
     for tag in all:
         chargespot = {"lat" : "" , "lng" : "" }
-        chargespot["lat"] = tag.select_one('lat').text
-        chargespot["lng"] = tag.select_one('lng').text
+        chargespot["lat"] = str(tag.select_one('lat').text)
+        chargespot["lng"] = str(tag.select_one('lng').text)
         chargespot_list.append(chargespot)
-
-
     return render(request, 'map/main.html', {"chargespot_list": chargespot_list})
 
 def map(request):
-
-
-
     url ='http://open.ev.or.kr:8080/openapi/services/EvCharger/getChargerInfo?serviceKey=s7Ytkl8dJDy32JsmhtlyMEGVjWPfEcBuXNnDCYQHitUBkHblPkhsXakF6aMhFf6NFOcxj6RFnuim5wTJUPNrkQ%3D%3D'
     res = requests.get(url)
     res.encoding = None
@@ -37,9 +33,9 @@ def map(request):
     chargespot_list = []
     for tag in all:
         chargespot = {"lat" : "" , "lng" : "" }
-        chargespot["lat"] = tag.select_one('lat').text
-        chargespot["lng"] = tag.select_one('lng').text
+        chargespot["lat"] = str(tag.select_one('lat').text)
+        chargespot["lng"] = str(tag.select_one('lng').text)
         chargespot_list.append(chargespot)
-
-    # return render(request, 'map/map.html', {"chargespot_list_rat": chargespot_list[0]["rat"], "chargespot_list_lng": chargespot_list[0]["lng"]})
     return render(request, 'map/map.html', {"chargespot_list": chargespot_list} )
+
+
