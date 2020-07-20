c1d815f86b278eaa0e7d1c80564a6eaf6397f102
ssea9977 <ssea9977@gmail.com>
2020-07-20 14:27:42 +0900

    third

--- map/views.py
+++ map/views.py
@@ -2,9 +2,44 @@ from django.shortcuts import render
 from django.urls import path
 # from . import data
 from django.template import loader
+from bs4 import BeautifulSoup
+import requests
+
+
 
 def main(request):
-    return render(request, 'map/main.html', {})
+    url ='http://open.ev.or.kr:8080/openapi/services/EvCharger/getChargerInfo?serviceKey=s7Ytkl8dJDy32JsmhtlyMEGVjWPfEcBuXNnDCYQHitUBkHblPkhsXakF6aMhFf6NFOcxj6RFnuim5wTJUPNrkQ%3D%3D'
+    res = requests.get(url)
+    res.encoding = None
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # print(res.text)
+    all = soup.select('item')
+    chargespot_list = []
+    for tag in all:
+        chargespot = {"lat" : "" , "lng" : "" }
+        chargespot["lat"] = tag.select_one('lat').text
+        chargespot["lng"] = tag.select_one('lng').text
+        chargespot_list.append(chargespot)
+
+
+    return render(request, 'map/main.html', {"chargespot_list": chargespot_list})
 
 def map(request):
-    return render(request, 'map/map.html', {})
+
+
+
+    url ='http://open.ev.or.kr:8080/openapi/services/EvCharger/getChargerInfo?serviceKey=s7Ytkl8dJDy32JsmhtlyMEGVjWPfEcBuXNnDCYQHitUBkHblPkhsXakF6aMhFf6NFOcxj6RFnuim5wTJUPNrkQ%3D%3D'
+    res = requests.get(url)
+    res.encoding = None
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # print(res.text)
+    all = soup.select('item')
+    chargespot_list = []
+    for tag in all:
+        chargespot = {"lat" : "" , "lng" : "" }
+        chargespot["lat"] = tag.select_one('lat').text
+        chargespot["lng"] = tag.select_one('lng').text
+        chargespot_list.append(chargespot)
+
+    # return render(request, 'map/map.html', {"chargespot_list_rat": chargespot_list[0]["rat"], "chargespot_list_lng": chargespot_list[0]["lng"]})
+    return render(request, 'map/map.html', {"chargespot_list": chargespot_list} )
