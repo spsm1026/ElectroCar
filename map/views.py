d9c7146255530a5780b66a08ae803bf8516f0a22
spsm1026 <ppsm1026@gmail.com>
2020-07-20 18:14:12 +0900

    0720 6:14 map.html ajax로 수정

--- map/views.py
+++ map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.urls import path
 # from . import data
 from django.template import loader
@@ -23,9 +24,9 @@ def main(request):
     return render(request, 'map/main.html', {"chargespot_list": chargespot_list})
 
 def map(request):
+    return render(request, 'map/map.html')
 
-
-
+def map_data(request):
     url ='http://open.ev.or.kr:8080/openapi/services/EvCharger/getChargerInfo?serviceKey=s7Ytkl8dJDy32JsmhtlyMEGVjWPfEcBuXNnDCYQHitUBkHblPkhsXakF6aMhFf6NFOcxj6RFnuim5wTJUPNrkQ%3D%3D'
     res = requests.get(url)
     res.encoding = None
@@ -43,4 +44,4 @@ def map(request):
             chargespot_list.append(chargespot)
 
     # return render(request, 'map/map.html', {"chargespot_list_rat": chargespot_list[0]["rat"], "chargespot_list_lng": chargespot_list[0]["lng"]})
-    return render(request, 'map/map.html', {"chargespot_list": chargespot_list} )
+    return JsonResponse(chargespot_list, safe=False)
