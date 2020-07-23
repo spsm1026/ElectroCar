25811a55216e4fb2530225fdbba2d4e9a70ed403
spsm1026 <ppsm1026@gmail.com>
2020-07-23 17:40:22 +0900

    aa

--- customer/models.py
+++ customer/models.py
@@ -6,3 +6,8 @@ class User(models.Model):
     useremail = models.CharField(max_length=200)
     password = models.EmailField(max_length=200)
     cars = models.CharField(max_length=200)
+
+# class user_bookmark(models.Model):
+#     user_id = ForeignKey(User, on_delete=models.CASCADE)
+#     bookmark_name = CharField(max_length=200)
+#     bookmark_address = CharField(max_length=200)
